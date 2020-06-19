41ed2c4d26e8a2938b32280a89f1ad236a451cab
Dusten Knull <dakatk97@gmail.com>
2020-06-19 15:25:45 -0500

    Added solution and project files, created basic project structure

---
+++ Business/Behavior/ISerialized.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Project0.Business.Behavior {
+    interface ISerialized {
+
+        void Serialize (File file);
+        void Deserialize (File file);
+    }
+}

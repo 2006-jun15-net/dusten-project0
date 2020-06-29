4284c5a17ee784706b5b4570323e4ae50cc70c60
Dusten Knull <dakatk97@gmail.com>
2020-06-29 11:33:48 -0500

    Added store repository test

--- Project0.Test/DataAccess/Repository/StoreRepositoryTest.cs
+++ Project0.Test/DataAccess/Repository/StoreRepositoryTest.cs
@@ -3,12 +3,22 @@ using Xunit;
 
 namespace Project0.Test.DataAccess.Repository {
 
-    public class StoreRepositoryTest {
+    public class StoreRepositoryTest : RepositoryTest {
 
         private readonly StoreRepository mStoreRepository;
 
-        public StoreRepositoryTest () {
+        public StoreRepositoryTest () : base () {
+            mStoreRepository = new StoreRepository (mOptions);
+        }
 
+        [Fact]
+        public void TestFindByName () {
+
+            var storeByName = mStoreRepository.FindByName ("Milk and Cheese");
+            var storeById = mStoreRepository.FindById (1);
+
+            Assert.Equal (storeByName.Name, storeById.Name);
+            Assert.Equal (storeById.Id, storeByName.Id);
         }
     }
 }
